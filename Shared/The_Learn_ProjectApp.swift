f579f987c36acdaab0c935570331dd2682ef4cf3
Ranga Reddy <ranga@aarth.io>
2022-01-07 14:35:36 +0530

    Initial Commit

---
+++ Shared/The_Learn_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Learn_ProjectApp.swift
+//  Shared
+//
+//  Created by Ranga Reddy Nukala on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Learn_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
